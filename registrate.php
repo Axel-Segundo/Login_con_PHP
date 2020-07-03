4bd8063a2b5f9e541ffb2033206ea7c23774e888
Axel Segundo <axeljasegundo@gmail.com>
2020-07-03 15:23:50 -0500

    Validando y encriptando

--- registrate.php
+++ registrate.php
@@ -9,7 +9,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $password = $_POST['password'];
     $password2 = $_POST['password2'];
 
-    echo "$usuario . $password . $password2 ";
 
     $errores = '';
 
@@ -29,6 +28,20 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         if ($resultado != false) {
             $errores .= '<li>El nombre de usuario ya existe</li>';
         }
+
+        $password = hash('sha512', $password);
+        $password2 = hash('sha512', $password2);
+
+        if ($password != $password2) {
+            $errores .= '<li>Las contraseñas no son iguales</li>';
+        }
+    }
+
+    if ($errores == '') {
+        $statement = $conexion->prepare('INSERT INTO usuarios (id, usuario, pass) VALUES (null, :usuario, :pass)');
+        $statement->execute(array(':usuario' => $usuario, ':pass' => $password));
+
+        header('Location: login.php');
     }
 }
 
